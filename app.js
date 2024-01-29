7f1ba41f5597843c6badad525d8a0b995690a89f
Irfan Yilmaz <iy.usa@outlook.com>
2024-01-28 23:32:41 -0500

    finish migrating index.js to app.js

--- app.js
+++ app.js
@@ -1,14 +1,32 @@
 const express = require('express')
+const path = require('path');
+
 const app = express();
-const port = 3000;
+const port = 8080;
+
+
+app.use((req, res, next) => {
+    console.log("Middleware Active");
+    next();
+});
 
 
 app.get("/", (req, res) => {
-    res.send("App.js response object")
+    res.sendFile(path.join(__dirname, '/index.html'));
 })
+
+app.get("/about", (req, res) => {
+    res.sendFile(path.join(__dirname, '/about.html'));
+})
+
+app.get("/contact", (req, res) => {
+    res.sendFile(path.join(__dirname, './contact-me.html'));
+})
+
 
 
 app.listen(port, () => {
     console.log(`App listening on port ${port}: http://localhost:${port}`);
 })
+
 
